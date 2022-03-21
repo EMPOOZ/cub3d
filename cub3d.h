36b424c792aa6edc82dc22c5446c084cc6a74d96
Retta Micheline <rmicheli@si-e1.kzn.21-school.ru>
2022-03-21 17:50:33 +0300

    c

--- cub3d.h
+++ cub3d.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmicheli <rmicheli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 17:50:14 by rmicheli          #+#    #+#             */
+/*   Updated: 2022/03/21 17:50:14 by rmicheli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
 # define CUB3D_H
 # include <unistd.h>
@@ -7,16 +19,16 @@
 # include <gnl/get_next_line.h>
 # include <libft/libft.h>
 
-typedef struct	s_zone
+typedef	struct s_zone
 {
 	int		image_y;
 	int		image_x;
-	char	*NO;
-	char	*SO;
-	char	*WE;
-	char	*EA;
-	char	*F;
-	char	*C;
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+	char	*f;
+	char	*v;
 	char	**matrice;
 }				t_zone;
 
